63330c2fbe752e14775d9aec4476c1982065f020
Imanuel Ulbricht <me@imanuel.dev>
2024-05-20 18:13:19 +0000

    Bump androidx.compose:compose-bom from 2024.02.01 to 2024.05.00
    Bumps androidx.compose:compose-bom from 2024.02.01 to 2024.05.00.

--- detection/build.gradle.kts
+++ detection/build.gradle.kts
@@ -31,7 +31,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
     implementation("androidx.activity:activity-compose:1.9.0")
-    implementation(platform("androidx.compose:compose-bom:2024.02.01"))
+    implementation(platform("androidx.compose:compose-bom:2024.05.00"))
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
