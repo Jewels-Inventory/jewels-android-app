e6652d9d00dda184ef267b1550a7de5ef38508df
Imanuel Ulbricht <imanuel.ulbricht@jinya.de>
2024-05-26 04:41:51 +0200

    Fix build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     kotlin("android")
     kotlin("plugin.serialization")
+    kotlin("plugin.compose")
 }
 
 val jewelsMajorVersion: String by project
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
 
     kotlin("android") version "2.0.0" apply false
     kotlin("plugin.serialization") version "2.0.0" apply false
+    kotlin("plugin.compose") version "2.0.0" apply false
 }
