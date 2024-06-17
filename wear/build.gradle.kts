7eba8ccce85e3648fbc143543dad977c6cc2ad5d
Imanuel Ulbricht <imanuel.ulbricht@jinya.de>
2024-06-18 01:55:21 +0200

    CHORE: Bump dependencies

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -88,7 +88,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
     implementation("androidx.activity:activity-compose:1.9.0")
-    implementation(platform("androidx.compose:compose-bom:2024.05.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.06.00"))
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
