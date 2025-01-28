1e6877ab31d7e34ba4e4f5c082dff7ef139e3678
Imanuel Ulbricht <me@imanuel.dev>
2025-01-29 00:41:46 +0100

    CHORE: Bump dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -97,10 +97,10 @@ dependencies {
     implementation(project(":detection"))
 
     implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.activity:activity-compose:1.10.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
 
-    implementation(platform("androidx.compose:compose-bom:2024.12.01"))
+    implementation(platform("androidx.compose:compose-bom:2025.01.00"))
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
@@ -113,8 +113,8 @@ dependencies {
     implementation("androidx.camera:camera-view:1.4.1")
     implementation("com.google.mlkit:barcode-scanning:17.3.0")
     implementation("com.google.accompanist:accompanist-permissions:0.37.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
-    implementation("androidx.compose.material3.adaptive:adaptive-android:1.1.0-alpha08")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
+    implementation("androidx.compose.material3.adaptive:adaptive-android:1.1.0-alpha09")
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-scalars:2.11.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
@@ -125,7 +125,7 @@ dependencies {
     implementation("androidx.work:work-runtime:2.10.0")
     implementation("androidx.work:work-runtime-ktx:2.10.0")
 
-    implementation(platform("io.insert-koin:koin-bom:4.0.1"))
+    implementation(platform("io.insert-koin:koin-bom:4.0.2"))
     implementation("io.insert-koin:koin-core")
     implementation("io.insert-koin:koin-core-coroutines")
     implementation("io.insert-koin:koin-compose")
