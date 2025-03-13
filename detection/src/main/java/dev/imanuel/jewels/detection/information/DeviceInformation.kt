18bceaac6e21c6a75899739ca2cf5540ac6e4b0b
Imanuel Ulbricht <me@imanuel.dev>
2025-03-13 01:21:11 +0100

    CHORE: Bump dependencies

--- detection/src/main/java/dev/imanuel/jewels/detection/information/DeviceInformation.kt
+++ detection/src/main/java/dev/imanuel/jewels/detection/information/DeviceInformation.kt
@@ -1,6 +1,8 @@
 package dev.imanuel.jewels.detection.information
 
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonIgnoreUnknownKeys
 
 @Serializable
 enum class DeviceType {
@@ -14,9 +16,13 @@ data class Drive(val name: String, val driver: String, val manufacturer: String,
 @Serializable
 data class Cpu(val manufacturer: String, val model: String, val speed: Float?, val cores: Int, val threads: Int)
 
+@OptIn(ExperimentalSerializationApi::class)
+@JsonIgnoreUnknownKeys
 @Serializable
 data class Mainboard(val manufacturer: String, val version: String, val model: String)
 
+@OptIn(ExperimentalSerializationApi::class)
+@JsonIgnoreUnknownKeys
 @Serializable
 data class Kernel(val version: String, val architecture: String)
 
--- wear/src/main/proguard-rules.pro
+++
@@ -1,1 +0,0 @@
--keep class dev.imanuel.jewels.detection.KoinKt
