780d55fbaa7cde84b64255f4bf1415f8705d650c
Imanuel Ulbricht <me@imanuel.dev>
2026-02-20 21:36:55 +0100

    JWLS-21: Add mlkit to proguard

--- app/src/main/java/dev/imanuel/jewels/utils/qr.kt
+++ app/src/main/java/dev/imanuel/jewels/utils/qr.kt
@@ -1,21 +1,21 @@
 package dev.imanuel.jewels.utils
 
+import android.content.Context
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.platform.LocalContext
 import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions
 import com.google.mlkit.vision.codescanner.GmsBarcodeScanning
 import kotlinx.serialization.json.Json.Default.decodeFromString
+import org.koin.compose.koinInject
 
 @Composable
 fun rememberQrScanner(
+    context: Context = koinInject(),
     onResult: (String) -> Unit,
     onCancel: () -> Unit = {},
     onError: (Exception) -> Unit = {}
 ): () -> Unit {
-    val context = LocalContext.current
-
     val scanner = remember {
         val options = GmsBarcodeScannerOptions.Builder()
             .setBarcodeFormats(Barcode.FORMAT_QR_CODE)
@@ -43,11 +43,12 @@ fun rememberQrScanner(
 
 @Composable
 inline fun <reified T> rememberJsonQrScanner(
+    context: Context = koinInject(),
     crossinline onResult: (T) -> Unit,
     noinline onCancel: () -> Unit = {},
     noinline onError: (Exception) -> Unit = {}
 ): () -> Unit {
-    return rememberQrScanner({ value ->
+    return rememberQrScanner(context, { value ->
         val result = decodeFromString<T>(value)
         onResult(result)
     }, onCancel, onError)
